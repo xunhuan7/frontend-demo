9e1b3d271f2978b2da1e394f3399d0d3e60d7bdf
Jason.Deng <676081195@qq.com>
2017-11-22 14:08:03 +0800

    事件的监听，捕获，冒泡和委托

--- defineEvent/defineEvent.js
+++ defineEvent/defineEvent.js
@@ -1,8 +1,18 @@
 /**
  * Created by Thinkpad on 2017/9/21.
+ * 事件监听：
+ *          可以绑定多个事件，顺序执行
+ *          更方便的移除事件
+ *          老版IE的事件监听函数中，this指向window
+ * 事件冒泡和事件捕获：
+ *         控制事件触发流程，首先从父到子（事件捕获），找到触发事件的最精确的元素（事件目标target），然后从子到父触发事件（事件冒泡）
+ *         一般采用事件冒泡模型，阻止事件冒泡用event.stopPropagation(),IE用event.cancelBubble = true
+ * 事件委托：
+ *         event.target.nodeName.toLowerCase()  // 标准
+ *         event.srcElement.nodeName.toLowerCase()  // IE
  */
 
-// 方法一：判断绑定（每次调用都需要进行浏览器检测）
+// 方法一：判断绑定（每次调用都需要进行功能检测）
 function addHandler(target, type, handler) {
     if (target.addEventListener) {//支持DOM2
         addHandler = function (target, type, handler) {
